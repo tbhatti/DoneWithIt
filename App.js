ee652a7ab5481fbab396237d2337e141d4c71411
Tanzeem Akhtar Bhatti <tbhatti007@gmail.com>
2020-08-10 07:30:56 +0800

    Firt React Native app

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert,
+  Platform, // For IOS and Android, because SafeAreaView does not work in Android, you must have to give padding top
+} from "react-native";
 
 export default function App() {
+  const handlePrss = () => {};
+  let x = 1;
+  console.log("Text Pressed Here", require("./assets/icon.png"));
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    /*If in style we have array  then secod style will override first style of array*/
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Text>Hello World</Text>
+      <TouchableHighlight onPress={() => console.log("Image Tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 200,
+            uri:
+              "https://tse3-mm.cn.bing.net/th/id/OIP.Cmc_jOu2fMPKu-2xEvxduQHaF4?w=261&h=207&c=7&o=5&dpr=1.5&pid=1.7",
+          }}
+        />
+      </TouchableHighlight>
+      <Button
+        color="orange"
+        title="Click Me!"
+        onPress={() =>
+          Alert.alert("My Title", "My Message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
+      <Button
+        color="red"
+        title="Click Me!"
+        onPress={() =>
+          Alert.prompt("My Title", "My Message", (text) => console.log(text))
+        }
+      />
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          height: 70,
+          width: 200,
+        }}
+      ></View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
+const containerStyle = { backgroundColor: "yellow" };
 const styles = StyleSheet.create({
+  // Stylesheet.create() 1. Validates the properties used inside container
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? 20 : 0,
   },
 });
